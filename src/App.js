20446c8e0883437486d8e50e1cd296dd5cb6ccd4
Raju <rajuraj16520@gmail.com>
2022-11-14 15:12:14 +0530

    design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React from "react";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from "./screens/Home";
+import Playground from "./screens/Playground";
+import Error404 from "./screens/Error404";
+import { GlobalStyle } from "./style/global";
+import LeftComponent from "./screens/Home/LeftComponent";
+import RightComponent from "./screens/Home/RightComponent";
 
 function App() {
   return (
-    <div className="App">
-code ide
-    </div>
+
+    <BrowserRouter>
+    <GlobalStyle />
+    <Routes>
+      <Route path="/" element ={<Home />} />
+      <Route path="Playground" element ={<Playground />} />
+      <Route path="*" element={<Error404 />} />
+    </Routes>
+    </BrowserRouter>
+
   );
 }
 
